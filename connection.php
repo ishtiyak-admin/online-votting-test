eba71682eee4e72d0713fe4fea8b3bc58efd40bb
Ishtiyak Ansari <support@devtechnosys.info>
2020-04-23 18:40:12 +0530

    55

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php 
-$con = mysqli_connect("localhost","root","","polltest") or die ("error" . mysqli_error($con));
+$con = mysqli_connect("localhost","ishtiyak","","demo") or die ("error" . mysqli_error($con));
 ?>
